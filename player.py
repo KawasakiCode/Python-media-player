f57b56e2a4a423cdc48e3e577780ce4b5b3d7cfb
Pantelis Remvos <remvosp@gmail.com>
2024-11-11 18:29:43 +0200

    all buttons work + time labels show correct time

--- player.py
+++ player.py
@@ -9,11 +9,16 @@ class MusicPlayer:
         self.current_song = None
         self.song_list = []
         self.current_position = 0
-        self.is_paused = False
+        self.is_paused = True
+        self.song_length = 0
     
     def load_song(self, song_paths):
+        if not song_paths:
+            print("Error: No songs found")
+            return
         self.song_list = song_paths
         self.current_song = song_paths[0]
+        self.set_song_length()
     
     def play_song(self):
         if not self.is_playing:
@@ -21,29 +26,48 @@ class MusicPlayer:
             pygame.mixer.music.play()
             self.is_playing = True
             self.is_paused = False
-        else: 
+            self.set_song_length()
+        elif self.is_paused:
             pygame.mixer.music.unpause()
+            self.is_paused = False
     
     def pause_song(self):
-        if self.is_playing:
+        if self.is_playing and not self.is_paused:
             pygame.mixer.music.pause()
-            self.is_playing = False
             self.is_paused = True
     
     def next_song(self):
         current_index = self.song_list.index(self.current_song)
         next_index = (current_index + 1) % len(self.song_list)
         self.current_song = self.song_list[next_index]
+        self.is_playing = False
         self.play_song()
     
     def previous_song(self):
         current_index = self.song_list.index(self.current_song)
-        prev_index = (current_index - 1) % len(self.song_list)
+        if(current_index == 0):
+            prev_index = len(self.song_list) - 1
+        else:
+            prev_index = (current_index - 1) % len(self.song_list)
         self.current_song = self.song_list[prev_index]
+        self.is_playing = False
         self.play_song()
 
-    def get_current_position(self):
-        return pygame.mixer.music.get_pos() / 1000 + self.current_position
+    def get_song_position(self):
+        milliseconds = pygame.mixer.music.get_pos()
+        seconds = milliseconds // 1000
+        minutes = seconds // 60
+        seconds = seconds % 60
+        return f"{minutes}:{seconds:02}"
     
     def set_position(self, position):
         pygame.mixer.music.play(start = position)
+
+    def get_length(self):
+        minutes = self.song_length // 60
+        seconds = self.song_length % 60
+        return f"{minutes}:{seconds:02}"
+    
+    def set_song_length(self):
+        sound = pygame.mixer.Sound(self.current_song)
+        self.song_length = int(sound.get_length())
--- ui.py
+++ ui.py
@@ -3,8 +3,13 @@ from tkinter import ttk
 from tkinter import *
 from PIL import Image, ImageTk
 from player import MusicPlayer
+import os
 
 music_player = MusicPlayer()
+
+DIRECTORY = r"D:\Pantelis\playlist"
+SONGS_LIST = [os.path.join(DIRECTORY, file) for file in os.listdir(DIRECTORY)]
+music_player.load_song(SONGS_LIST)
 
 root = Tk()
 root.title("Spotify alla better")
@@ -23,8 +28,8 @@ resized_image = original_pause.resize((new_width, new_height), Image.LANCZOS)
 PAUSE_IMAGE = ImageTk.PhotoImage(resized_image)
 pause = tk.Button(root, bd = 0, cursor = "hand2", 
                   image = PAUSE_IMAGE, borderwidth = 0, 
-                  highlightthickness = 0, relief = "flat", activebackground= "black")
-pause.place(x = 940, y = 915)
+                  highlightthickness = 0, relief = "flat", activebackground= "black", command = music_player.pause_song)
+pause.place(x = 940, y = 850)
 
 original_next = Image.open("Assets/Next_song_button.png")
 
@@ -37,7 +42,7 @@ resized_next = original_next.resize((new_next_width, new_next_height), Image.LAN
 
 NEXT_IMAGE = ImageTk.PhotoImage(resized_next)
 next = tk.Button(root, bd = 0, cursor = "hand2", image = NEXT_IMAGE, borderwidth = 0,
-    highlightthickness = 0,  relief="flat", activebackground= "black")
+    highlightthickness = 0,  relief="flat", activebackground= "black", command = music_player.next_song)
 next.place(x = 1010, y = 920)
 
 original_previous = Image.open("Assets/Previous_song_button.png")
@@ -48,7 +53,7 @@ resized_previous = original_previous.resize((new_previous_width, new_previous_he
 
 PREVIOUS_IMAGE = ImageTk.PhotoImage(resized_previous)
 previous = tk.Button(root, bd = 0, cursor = "hand2", image = PREVIOUS_IMAGE, borderwidth = 0, 
-    highlightthickness = 0,  relief="flat", activebackground= "black")
+    highlightthickness = 0,  relief="flat", activebackground= "black", command = music_player.previous_song)
 previous.place(x = 880, y = 920)
 
 original_play = Image.open("Assets/play_button.png")
@@ -63,18 +68,31 @@ resized_play = original_play.resize((new_play_width, new_play_height), Image.LAN
 
 PLAY_IMAGE = ImageTk.PhotoImage(resized_play)
 play = tk.Button(root, bd = 0, cursor = "hand2", image = PLAY_IMAGE, borderwidth = 0, highlightthickness = 0,
-                 relief = "flat", activebackground = "black")
+                 relief = "flat", activebackground = "black", command = music_player.play_song)
 play.place(x = 935, y = 910)
 
-time_label = Label(root, text = "00:00", background = "black", height = 10, width = 25, 
+def update_time():
+    if music_player.is_playing:
+        current_time = music_player.get_song_position()
+        end_time = music_player.get_length()
+
+        time_label.config(text = current_time)
+        end_time_label.config(text = end_time)
+        
+    root.after(1000, update_time)
+
+time_label = Label(root, text = "0:00", background = "black", height = 10, width = 25, 
                     bd = 0, font = ("Arial", 10), relief = "flat", fg="gray")
 time_label.place(x = 510, y = 910)
 
-end_time_label = Label(root, text = "4:00", background = "black", height = 10, width = 25,
+end_time = music_player.get_length()
+end_time_label = Label(root, text = "0:00", background = "black", height = 10, width = 25,
                         bd = 0, font = ("Arial", 10), relief = "flat", fg = "gray")
 end_time_label.place(x = 1210, y = 910)
 
 length_slider = ttk.Scale(root, from_=0, to=120, orient = "horizontal", length = 655)
 length_slider.place(x = 635, y = 977)
+
+update_time()
 
 root.mainloop()
