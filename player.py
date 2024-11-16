ad34755aa09f175193dbb38a9cea7426936ad247
Pantelis Remvos <remvosp@gmail.com>
2024-11-16 17:42:00 +0200

    implemented shuffle and correct previous song function

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.mixer.init()
 
@@ -7,17 +8,24 @@ class MusicPlayer:
         self.is_playing = False
         self.current_song = None
         self.song_list = []
+        self.shuffle_list = []
         self.current_position = 0
         self.is_paused = True
         self.song_length = 0
-    
+        self.first_go = True
+        self.last_played_index = None
+
     def load_song(self, song_paths):
         if not song_paths:
             print("Error: No songs found")
             return
         self.song_list = song_paths
-        self.current_song = song_paths[0]
+        index = random.randint(0, len(self.song_list) - 1)
+        self.current_song = song_paths[index]
         self.set_song_length()
+        for i in range(len(self.song_list) - 1):
+            self.shuffle_list.append(0)
+        self.shuffle_list[index] = 1
     
     def play_song(self):
         if not self.is_playing:
@@ -36,21 +44,65 @@ class MusicPlayer:
             self.is_paused = True
     
     def next_song(self):
-        current_index = self.song_list.index(self.current_song)
-        next_index = (current_index + 1) % len(self.song_list)
-        self.current_song = self.song_list[next_index]
-        self.is_playing = False
-        self.play_song()
+        next_index = random.randint(0, len(self.song_list) - 1)
+        while(True):
+            if self.shuffle_list[next_index] == 0:
+                self.current_song = self.song_list[next_index]
+                self.is_playing = False
+                self.shuffle_list[next_index] = max(self.shuffle_list) + 1
+                #print(f"Song that is playing {next_index}")
+                self.play_song()
+                break
+            else:
+                counter = 0
+                for i in range(len(self.song_list) - 1):
+                    if(self.shuffle_list != 0):
+                        counter += 1
+                    else: 
+                        break
+                if(counter == len(self.shuffle_list) - 1):
+                    for i in range(len(self.song_list) - 1):
+                        self.shuffle_list[i] = 0
+                next_index = random.randint(0, len(self.song_list) - 1)
     
     def previous_song(self):
-        current_index = self.song_list.index(self.current_song)
-        if(current_index == 0):
-            prev_index = len(self.song_list) - 1
+        if(max(self.shuffle_list) == 0):
+            prev_index = random.randint(0, len(self.shuffle_list) - 1)
+            self.current_song = self.song_list[prev_index]
+            self.is_playing = False
+            self.last_played_index = prev_index
+            #print(f"All zero and prev index {prev_index}")
+            self.play_song()
+        elif(self.first_go and max(self.shuffle_list) == 1):
+            prev_index = random.randint(0, len(self.shuffle_list) - 1)
+            self.shuffle_list[self.shuffle_list.index(max(self.shuffle_list))] = 0
+            self.first_go = False
+            self.current_song = self.song_list[prev_index]
+            self.is_playing = False
+            #print(f"First time loading so max is 1 {prev_index}")
+            self.last_played_index = prev_index
+            self.play_song()
         else:
-            prev_index = (current_index - 1) % len(self.song_list)
-        self.current_song = self.song_list[prev_index]
-        self.is_playing = False
-        self.play_song()
+            if(max(self.shuffle_list) == 1):
+                #print("Max is one")
+                self.shuffle_list[self.shuffle_list.index(max(self.shuffle_list))] = 0
+                #print("Max is zero again")
+                #print(self.last_played_index)
+                if(self.last_played_index is not None):
+                    prev_index = self.last_played_index
+                    self.current_song = self.song_list[prev_index]
+                    self.is_playing = False
+                    self.play_song()
+                else:
+                    print("Problem last played index is None")
+            else:
+                self.shuffle_list[self.shuffle_list.index(max(self.shuffle_list))] = 0
+                prev_index = self.shuffle_list.index(max(self.shuffle_list))
+                self.current_song = self.song_list[prev_index]
+                self.is_playing = False
+                #print(f"Not first time and max not zero or 1 and prev index {prev_index}")
+                self.play_song()
+    
 
     def get_song_position(self):
         milliseconds = pygame.mixer.music.get_pos()
